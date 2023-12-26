41df48a25527220d9defc4a9de5b97b7e8e53ade
breno.mcosta <breno.mcosta@pbh.gov.br>
2023-12-26 10:21:01 -0300

    up

--- src/main/java/com/br/aws/ecommerce/product/ProductsAdminFunction.java
+++ src/main/java/com/br/aws/ecommerce/product/ProductsAdminFunction.java
@@ -5,54 +5,74 @@ import static software.amazon.lambda.powertools.tracing.CaptureMode.DISABLED;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.br.aws.ecommerce.layers.base.BaseLambdaFunction;
+import com.br.aws.ecommerce.layers.model.ErrorMessageDTO;
+import com.br.aws.ecommerce.layers.model.ProductDTO;
+import com.br.aws.ecommerce.layers.repository.ProductRepository;
 
 import software.amazon.lambda.powertools.metrics.Metrics;
 import software.amazon.lambda.powertools.tracing.Tracing;
 
-public class ProductsAdminFunction
+public class ProductsAdminFunction extends BaseLambdaFunction
 		implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+	
 
 	@Tracing(captureMode = DISABLED)
 	@Metrics(captureColdStart = true)
 	public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
 		
-		LambdaLogger logger = context.getLogger();
+		final LambdaLogger logger = context.getLogger();
 		
 		Map<String, String> headers = new HashMap<>();
 		headers.put("Content-Type", "application/json");
 		headers.put("X-Custom-Header", "application/json");
-
 		final APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent().withHeaders(headers);
 		
-		if ("/products".equals(input.getResource())) {
-			
-			  String output = String.format("{ \"message\": \"POST\"}");
-			
-			  return response.withStatusCode(201).withBody(output);
-		} else if ("/products/{id}".equals(input.getResource())) {
-			
-			final String id = input.getPathParameters() != null ? input.getPathParameters().get("id") : null;
-			
-			if ("PUT".equals(input.getHttpMethod())) {
-				   String output = String.format("{ \"message\": \"PUT\" }");
-				
-				 return response.withStatusCode(200).withBody(output);
-				
-			} else if ("DELETE".equals(input.getHttpMethod())) {
-				   String output = String.format("{ \"message\": \"DELETE\" }");
-				return response.withBody(output);
-			}
-			
-		}
-	 
+		try {
 
-		logger.log("Method not allowed");
-		return response.withBody("{ \"error\": \"method not allowed\" }").withStatusCode(405);
+			final ProductRepository productRepository = new ProductRepository(AmazonDynamoDBClientBuilder.defaultClient());
+			
+			if ("/products".equals(input.getResource())) {
+				
+				  final ProductDTO productBody = getMapper().readValue(input.getBody(), ProductDTO.class);	
+				
+				  return response.withStatusCode(201).withBody(getMapper().writeValueAsString(productRepository.save(productBody)));
+				  
+			} else if ("/products/{id}".equals(input.getResource())) {
+				
+				final String id = input.getPathParameters() != null ? input.getPathParameters().get("id") : null;
+				
+				if ("PUT".equals(input.getHttpMethod())) {
+					  
+					  final ProductDTO productBody = getMapper().readValue(input.getBody(), ProductDTO.class);	
+						
+					  return response.withStatusCode(200).withBody(getMapper().writeValueAsString(productRepository.update(productBody,id)));
+					
+				} else if ("DELETE".equals(input.getHttpMethod())) {
+					
+					productRepository.delete(id);
+					
+					return response.withStatusCode(204);
+				}
+			}
+		 
+
+			logger.log("Method not allowed");
+			return response.withBody(super.getMapper().writeValueAsString(new ErrorMessageDTO("Method not allowed"))).withStatusCode(405);
+			
+			
+		} catch (Exception e) {
+			logger.log("Error: " + e.getMessage());
+			return response.withBody(String.format("{ \"error\": \"%s\" }",e.getMessage())).withStatusCode(500);
+		}
+		
+		
 		 
 	}
 
--- src/main/java/com/br/aws/ecommerce/product/ProductsFetchFunction.java
+++ src/main/java/com/br/aws/ecommerce/product/ProductsFetchFunction.java
@@ -3,62 +3,67 @@ package com.br.aws.ecommerce.product;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.br.aws.ecommerce.layers.base.BaseLambdaFunction;
+import com.br.aws.ecommerce.layers.model.ErrorMessageDTO;
+import com.br.aws.ecommerce.layers.repository.ProductRepository;
 
 import software.amazon.lambda.powertools.metrics.Metrics;
 import software.amazon.lambda.powertools.tracing.CaptureMode;
 import software.amazon.lambda.powertools.tracing.Tracing;
 
-public class ProductsFetchFunction
+public class ProductsFetchFunction extends BaseLambdaFunction
 		implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-
- 
 
 	@Tracing(captureMode = CaptureMode.DISABLED)
 	@Metrics(captureColdStart = true)
 	public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
 
-		LambdaLogger logger = context.getLogger();
+		final LambdaLogger logger = context.getLogger();
 
-		Map<String, String> headers = new HashMap<>();
+		final Map<String, String> headers = new HashMap<>();
 		headers.put("Content-Type", "application/json");
 		headers.put("X-Custom-Header", "application/json");
 
-		// Identificador único associado a um cliente, independente do número de
-		// requisições do cliente
-		final String lamdaRequestId = context.getAwsRequestId();
-
-		// Identificador de cada requisição HTTP
-		final String apiRequestId = input.getRequestContext().getRequestId();
-		logger.log(String.format("apiRequestId: %s lamdaRequestId: %s" , apiRequestId, lamdaRequestId));
-
 		final APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent().withHeaders(headers);
 
-		if ("/products".equals(input.getResource())) {
+		try {
 
-			String output = String.format("{ \"message\": \"velho vagabundo!\"}");
+			// Identificador único associado a um cliente, independente do número de
+			// requisições do cliente
+			final String lamdaRequestId = context.getAwsRequestId();
 
-			return response.withStatusCode(200).withBody(output);
+			// Identificador de cada requisição HTTP
+			final String apiRequestId = input.getRequestContext().getRequestId();
+			logger.log(String.format("apiRequestId: %s lamdaRequestId: %s", apiRequestId, lamdaRequestId));
+			
+			
+			final ProductRepository productRepository = new ProductRepository(AmazonDynamoDBClientBuilder.defaultClient());
 
-		} else if ("/products/{id}".equals(input.getResource())) {
+			if ("/products".equals(input.getResource())) {
 
-			final String id = input.getPathParameters() != null ? input.getPathParameters().get("id") : null;
+				return response.withStatusCode(200).withBody(super.getMapper().writeValueAsString(productRepository.findAll()));
 
-			String output = String.format("{ \"product_id\": \"%s\" }", id);
+			} else if ("/products/{id}".equals(input.getResource())) {
 
-			return response.withStatusCode(200).withBody(output);
+				final String id = input.getPathParameters() != null ? input.getPathParameters().get("id") : null;
+
+				return response.withStatusCode(200).withBody(super.getMapper().writeValueAsString(productRepository.findById(id)));
+			}
+
+			logger.log("Method not allowed");
+			return response.withBody(super.getMapper().writeValueAsString(new ErrorMessageDTO("Method not allowed"))).withStatusCode(405);
+
+		} catch (Exception e) {
+			logger.log("Error: " + e.getMessage());
+			return response.withBody(String.format("{ \"error\": \"%s\" }", e.getMessage())).withStatusCode(500);
 		}
 
-		logger.log("Method not allowed");
-		return response.withBody("{ \"error\": \"method not allowed\" }").withStatusCode(405);
-
 	}
-
-	 
- 
 
 }
